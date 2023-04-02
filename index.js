f1c0f564e8ddc7c98a12b23b2e87c4bd0f308081
Luís Eduardo Ribeiro Guerra <31783838+luisrguerra@users.noreply.github.com>
2023-04-02 09:15:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+const readline = require('readline');
 const api = require('./api');
 
-let promptText = "Write a poem";
-let tokenTextSize = 256;
-
-(async() => {
+async function sendPrompt(promptText, tokenTextSize) {
     const response = await api.davinci(promptText,tokenTextSize)
     console.log(response.data.choices[0].text);
-})()
+ };
+
+let terminalReader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+terminalReader.question("Prompt:\n", function(answer) {
+    sendPrompt(answer,256)
+    terminalReader.close();
+});
+
+
+
