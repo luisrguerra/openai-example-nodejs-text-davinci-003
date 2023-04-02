6f616f0b4e45d96762f6c78f44f8c5d02909713a
Luís Eduardo Ribeiro Guerra <31783838+luisrguerra@users.noreply.github.com>
2023-04-02 13:00:39 -0300

    mais opções de modelos

--- api.js
+++ api.js
@@ -1,13 +1,29 @@
 require('dotenv').config()
 const { Configuration, OpenAIApi } = require("openai");
+// Key from environment variable
+const key = process.env['OPENAI_API_KEY'];
 const configuration = new Configuration({
-  apiKey: process.env['OPENAI_API_KEY'],
+  apiKey: key,
 });
 const openai = new OpenAIApi(configuration);
 
+const gptModelName = "text-davinci-003";
+/* 
+text-davinci-003: 4,097 tokens	 
+Can do any language task with better quality,
+longer output, and consistent instruction-following
+than the curie, babbage, or ada models.
+Also supports inserting completions within text.
+
+text-davinci-002: 4,097 tokens
+Similar capabilities to text-davinci-003 but
+trained with supervised fine-tuning instead
+of reinforcement learning
+*/
+
 async function davinci(promptText,tokenSize) {
     return await openai.createCompletion({
-        model: "text-davinci-003",
+        model: gptModelName,
         prompt: promptText,
         temperature: 0,
         max_tokens: tokenSize,
